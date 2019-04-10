5009a1473f846475806960ee15435ddbf9165b55
Maryana <amaryana@gmail.com>
2019-04-10 07:03:26 -0700

    add next page, css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,48 @@
-$(document).ready(function (){
-  $("form#tickets").submit(function(event){
-  
-    event.preventDefault();   
-    
-    var userMovie =$("select#movie").val();
-    var userTime = $("select#timeTickets").val();
-    var userAge = $("select#age").val();
-    var totalValue = parseInt(userTime) + parseInt(userAge)
+$(document).ready(function () {
+    $("form#tickets").submit(function (event) {
+
+        event.preventDefault();
+
+        var userMovie = $("select#movie").val();
+        var userTime = $("select#timeTickets").val();
+        var userAge = $("select#age").val();
+        var totalValue = parseInt(userTime) + parseInt(userAge)
 
 
 //back end logic
 
-function Tickets (name, time, age) {
-  this.name = name;
-  this.time = time;
-  this.age= age;
-}
+        function Tickets(name, time, age) {
+            this.name = name;
+            this.time = time;
+            this.age = age;
+        }
 
-var newUser = new Tickets (userMovie, userTime, userAge);
-var price = "";
+        var newUser = new Tickets(userMovie, userTime, userAge);
+        var price = "";
 
-var thanks= "Thanks for your booking. Below, you'll receive your confirmation details by email.";
+        var thanks = "Thanks for your booking. Below, you'll receive your confirmation details by email.";
 
 
-var getThePrice = function(total){
-  if (total < 6){
-   price = "$5";
- }else if (total < 8){
-    price = "$10";
-    }else{
-    price = "$20";
-    }
-  return price
-}
+        var getThePrice = function (total) {
+            if (total < 6) {
+                price = "$5";
+            } else if (total < 8) {
+                price = "$10";
+            } else {
+                price = "$20";
+            }
+            return price
+        }
 
-var resultPrice= getThePrice(totalValue);
-var result = "The movie you have choosen is : " + userMovie + ". The price is:  " + resultPrice;
+        var resultPrice = getThePrice(totalValue);
+        var result = "The movie you have choosen is : " + userMovie + ". The price is:  " + resultPrice;
 
-$("#priceOfTickets").text(result);
-$("#hiddenText").show();
-});
+        $("#priceOfTickets").text(result);
+        $("#hiddenText").show();
+        $("#buyTickets").click(function () {
+            $("#firstPage").fadeOut();
+            $("#thankYouPage").fadeIn();
+        });
+
+    });
 });
